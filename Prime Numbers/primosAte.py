31589851ca81ddbd98e9c50016897dea8d9de032
Marcos Reinan de Assis Conceição <marcosreinanac@gmail.com>
2014-08-16 23:08:22 -0300

    Create primosAte.py

---
+++ Prime Numbers/primosAte.py
@@ -0,0 +1,19 @@
+def primosAte(valorMaximo, show=None):
+    '''
+    retorna uma lista de primos
+    '''
+    listaDePrimos = []
+    numero = 1
+    while numero <= valorMaximo:
+        if all(numero % primo != 0 for primo in listaDePrimos):
+            if not(numero == 1):
+                listaDePrimos.append(numero)
+                if show:
+                    print(numero)
+        numero += 1
+    return(listaDePrimos)
+
+
+if __name__ == '__main__':
+    valorMaximo = int(raw_input('Digite o valor maximo de busca: '))
+    primosAte(valorMaximo, True)
